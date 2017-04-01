e8aa43f4c4c853a6607419fc00a47abb154a566d
Sam Pellegrene <pellegrenesam@gmail.com>
2017-04-01 15:04:16 -0500

    Initial Commit

---
+++ ObjCButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCButton
+//
+//  Created by Samuel Pellegrene on 4/1/17.
+//  Copyright © 2017 Samuel Pellegrene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjCButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjCButton
+//
+//  Created by Samuel Pellegrene on 4/1/17.
+//  Copyright © 2017 Samuel Pellegrene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
